7d35e63b15ea299f7aa8397d27fd2c5c584cb130
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2024-10-31 16:21:48 -0300

    feat: adding model movie

--- back_end/movies_api/models.py
+++ back_end/movies_api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from uuid import uuid4
 
 # Create your models here.
+class Movies(models.Model):
+    id_movie = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    title = models.CharField(max_length=400)
+    year_realease = models.IntegerField()
+    img_url = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"ID: {self.id_movie}, title: {self.title}, year: {self.year_realease}, img_url: {self.img_url}"
--- back_end/movies_api/views.py
+++ back_end/movies_api/views.py
@@ -5,7 +5,14 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
+from .models import Movies
+from .api.serializers import MoviesSerializer
+
 
 @api_view(['GET'])
 def get_movies(request):
-    return Response({'hello': 'world'})
+    if request.method != 'GET':
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    movies = Movies.objects.all()
+    serilizer = MoviesSerializer(movies, many=True)
+    return Response({'hello': serilizer.data})
