9a801e02208c38475bcc566eaefb0596f4c190f0
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2024-10-31 18:39:35 -0300

    feat: adding new column model

--- back_end/movies_api/models.py
+++ back_end/movies_api/models.py
@@ -5,10 +5,11 @@ from uuid import uuid4
 class Movies(models.Model):
     id_movie = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=400)
+    plot = models.TextField(default="")
     year_realease = models.IntegerField()
     img_url = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"ID: {self.id_movie}, title: {self.title}, year: {self.year_realease}, img_url: {self.img_url}"
+        return f"ID: {self.id_movie}, title: {self.title}, plot: {self.plot}, year: {self.year_realease}, img_url: {self.img_url}"
