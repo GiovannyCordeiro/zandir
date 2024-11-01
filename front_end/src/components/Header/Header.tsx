842dc781784ba9d678dd9217b8b65fa829f15935
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2024-10-31 23:42:12 -0300

    feat: header component

---
+++ front_end/src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+export default function Header(){
+  return(
+    <>
+      <header className="w-screen h-1/6 bg-gray-600 flex items-center justify-center">
+        <div className='w-5/6 '>
+          <h2>FILMES Lorem Ipsum</h2>
+          <p>descrição Lorem ipsum dolor</p>
+        </div>
+      </header>
+    </>
+  )
+}
