1c2810cb220bcaab5494b3726c50c00a8ba5888c
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2024-11-01 01:24:37 -0300

    feat: add responsive layout

--- front_end/src/components/Header/Header.tsx
+++ front_end/src/components/Header/Header.tsx
@@ -1,10 +1,10 @@
 export default function Header(){
   return(
     <>
-      <header className="w-screen h-1/6 bg-gray-600 flex items-center justify-center">
-        <div className='w-5/6 '>
-          <h2>FILMES Lorem Ipsum</h2>
-          <p>descrição Lorem ipsum dolor</p>
+      <header className="w-screen h-1/6 bg-gray-200 flex items-center justify-center">
+        <div className='w-5/6 md:w-4/6 lg:w-3/6'>
+          <h2 className="text-2xl font-bold md:text-3xl mb-2">🎥 SEARCH YOUR MOVIE HERE!</h2>
+          <p className="text-lg md:text-xl">✅ Search for your favorite movie quickly and easily!</p>
         </div>
       </header>
     </>
--- front_end/src/components/ListMovies/ListMovies.tsx
+++ front_end/src/components/ListMovies/ListMovies.tsx
@@ -22,6 +22,8 @@ export default function ListMovies(){
   const [inputMovie, setInputMovie] = useState<string>('');
   const [emptyMovie, setEmptyMovie] = useState<boolean>(false);
 
+  const [errorInput, setErrorInput] = useState<boolean>(false);
+
   // Pesquisa dos filmes de forma inicial
   const handleFetch = async () => {
     try {
@@ -40,6 +42,13 @@ export default function ListMovies(){
 
   // Pesquisa do filme
   const fetchMovie = async () => {
+
+    if (inputMovie.length === 0){
+      setErrorInput(true);
+      return;
+    }
+    setErrorInput(false);
+
     try {
       setError(null);
       const response = await api.get<MovieResponse>(`/v1/movies_api/search/${inputMovie.toLowerCase()}`);
@@ -72,14 +81,14 @@ export default function ListMovies(){
   return(
     <>
       <main className='w-screen h-5/6 flex flex-col items-center justify-center gap-12 '>
-        <div className="flex w-5/6 max-w-sm self-center space-x-2 ">
-          <Input className='rounded-xl' type="text" onChange={handlerChange} onKeyDown={keyHandler} placeholder="Pesquise seu filme aqui😊" />
-          <Button className='rounded-xl' type="submit" onClick={fetchMovie}>Pesquisar!</Button>
+        <div className="flex w-5/6 self-center justify-center space-x-2 md:w-4/6 lg:w-3/6">
+          <Input className={`rounded-xl h-12 text-base ${errorInput ? 'border-red-500': 'border-black'} xl:w-3/5`} type="text" onChange={handlerChange} onKeyDown={keyHandler} placeholder={`${errorInput ? 'Please... type your movie here 🤔': 'Type your movie! 😊'}`} />
+          <Button className='rounded-xl text-base h-12' type="submit" onClick={fetchMovie}>Search!</Button>
         </div>
-        <section className='w-5/6 h-4/6 flex flex-col gap-2'>
+        <section className='w-5/6 h-4/6 flex flex-col items-center gap-2 md:w-4/6 lg:w-3/6'>
           {emptyMovie && <p>Sorry, we couldn't find any movie with that name 😭</p>}
           {movies.map((movie) => (
-            <Card className='flex' key={movie.id_movie}>
+            <Card className='flex h-3/5' key={movie.id_movie}>
               <div className='w-2/6 rounded-xl bg-gray-300'>
                 <img className='w-full h-full rounded-xl' src={movie.img_url} alt={`Movie Picture ${movie.title}`} />
               </div>
